eccbe1b5ae18b44b1a245a3b2d4ccff7aded2b68
Eloisefdcloiretcher <77059508+Eloisefdcloiretcher@users.noreply.github.com>
2021-01-08 15:23:03 +0100

    Add files via upload

---
+++ data/Prfctureetsousprfecture_5.js
@@ -0,0 +1,10 @@
+var json_Prfctureetsousprfecture_5 = {
+"type": "FeatureCollection",
+"name": "Prfctureetsousprfecture_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": "PAIHABIT0000000016134451", "ID_COM": "SURFCOMM0000000028105249", "ORIGIN_NOM": "BDNyme", "NATURE": "Préfecture", "NOM": "Blois", "IMPORTANCE": "2", "NOM_CARTO": "B" }, "geometry": { "type": "Point", "coordinates": [ 1.32686581004095, 47.591446950430552 ] } },
+{ "type": "Feature", "properties": { "ID": "PAIHABIT0000000016138129", "ID_COM": "SURFCOMM0000000028105203", "ORIGIN_NOM": "BDNyme", "NATURE": "Sous-préfecture", "NOM": "Romorantin-Lanthenay", "IMPORTANCE": "3", "NOM_CARTO": "R-L" }, "geometry": { "type": "Point", "coordinates": [ 1.747258982783008, 47.358655523366018 ] } },
+{ "type": "Feature", "properties": { "ID": "PAIHABIT0000000016131686", "ID_COM": "SURFCOMM0000000028105364", "ORIGIN_NOM": "BDNyme", "NATURE": "Sous-prMfecture", "NOM": "Vendome", "IMPORTANCE": "3", "NOM_CARTO": "V" }, "geometry": { "type": "Point", "coordinates": [ 1.065346791726322, 47.792555798575435 ] } }
+]
+}
